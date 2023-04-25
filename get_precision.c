ffacc227e5454862e9a656c375783e6a125509d0
Sliman <Elmansourisliman@gmail.com>
2023-04-25 23:32:45 +0100

    fix get flags

--- get_precision.c
+++ get_precision.c
@@ -11,24 +11,24 @@
 
 int get_precision(const char *format, int *i, va_list list)
 {
-	int curr_i = *i + 1;
+	int curr_index = *i + 1;
 	int precision = -1;
 
-	if (format[curr_i] != '.')
+	if (format[curr_index] != '.')
 		return (precision);
 
 	precision = 0;
 
-	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
+	for (curr_index += 1; format[curr_index] != '\0'; curr_index++)
 	{
-		if (is_digit(format[curr_i]))
+		if (is_digit(format[curr_index]))
 		{
 			precision *= 10;
-			precision += format[curr_i] - '0';
+			precision += format[curr_index] - '0';
 		}
-		else if (format[curr_i] == '*')
+		else if (format[curr_index] == '*')
 		{
-			curr_i++;
+			curr_index++;
 			precision = va_arg(list, int);
 			break;
 		}
@@ -36,7 +36,7 @@ int get_precision(const char *format, int *i, va_list list)
 			break;
 	}
 
-	*i = curr_i - 1;
+	*i = curr_index - 1;
 
 	return (precision);
 }
--- get_size.c
+++ get_size.c
@@ -10,18 +10,18 @@
 
 int get_size(const char *format, int *i)
 {
-	int curr_i = *i + 1;
+	int curr_index = *i + 1;
 	int size = 0;
 
-	if (format[curr_i] == 'l')
+	if (format[curr_index] == 'l')
 		size = S_LONG;
-	else if (format[curr_i] == 'h')
+	else if (format[curr_index] == 'h')
 		size = S_SHORT;
 
 	if (size == 0)
-		*i = curr_i - 1;
+		*i = curr_index - 1;
 	else
-		*i = curr_i;
+		*i = curr_index;
 
 	return (size);
 }
